d57999b50b2b8032a9bcd4de7e4150bf9ba7fa44
ssomanathan <35579317+ssomanathan@users.noreply.github.com>
2018-03-30 16:31:36 +0100

    Changes

--- Iris-setosa.py
+++ Iris-setosa.py
@@ -1,7 +1,10 @@
 #Somu    30th March 2018
 #Project 2018: Programming and Scripting
 #Iris Data set: Iris Setosa anlaysis
-#Technical Reference : https://www.tutorialspoint.com/python_pandas/python_pandas_series.htm
+#Technical Reference : 
+# https://stackoverflow.com/
+# http://pandas.pydata.org/pandas-docs/version
+# https://www.tutorialspoint.com/python_pandas/python_pandas_series.htm
 
 import pandas as pd
 import numpy as np
@@ -33,57 +36,3 @@ s = pd.DataFrame(Lvalue, columns = ['Petal Length','Petal Width','Sepal Length',
 print (s.describe(include='all'))
 print (s.plot(x='Petal Length',y='Sepal Length'))
 plt.show()
-
-#df = pd.read_csv("data/iris.csv")
-#print (df)
-
-#print (Lattributes)
-
-# Define the variables. counter for each flower in the iris dataset
-# Gname = variable to hold flower names
-# Gp/slen = Variables to hold the petal and sepal length and width
-
-# counter1, counter2,counter3 = 1,1,1
-# Gplen,Gpwid,Gslen,Gswid = 0.0,0.0, 0.0,0.0
-# Gname,Gnamei,Gnameo = '','',''
-
-# # Function to print the name, petel length, petal width, Sepal length and sepal width
-# def printiris(line):
-#         Gplen = float(str(line.split(',')[0]))
-#         Gpwid = float(str(line.split(',')[1]))
-#         Gslen = float(str(line.split(',')[2]))
-#         Gswid = float(str(line.split(',')[3]))
-#         print ("{:04.2f}  {:04.2f}  {:04.2f}  {:04.2f}".format (Gplen,Gpwid,Gslen,Gswid))
-
-# # Store all the distinct flower names 
-# flowers = set()
-# with open("data/iris.csv") as f:
-#         for line in f:
-#             Gname   = str(line.split(',')[4]).rstrip()
-#             flowers.add(Gname)
-
-# #Read the file and print the petel /Sepal details for each flower 
-# with open("data/iris.csv") as f:
-#         for line in f:
-#             Gnameo   = str(line.split(',')[4]).rstrip()
-#             if Gnameo == 'Iris-virginica':
-#                 if counter1 == 1: 
-#                     print ("{:^20}".format(Gnameo))
-#                     printiris(line)
-#                     counter1 += 1
-#                 else:
-#                    printiris(line)
-#             if Gnameo == 'Iris-versicolor':
-#                 if counter2 == 1: 
-#                     print ("{:^20}".format(Gnameo))
-#                     printiris(line)
-#                     counter2 += 1
-#                 else:
-#                    printiris(line)
-#             if Gnameo == 'Iris-setosa':
-#                 if counter3 == 1: 
-#                     print ("{:^20}".format(Gnameo))
-#                     printiris(line)
-#                     counter3 += 1
-#                 else:
-#                    printiris(line)                    
